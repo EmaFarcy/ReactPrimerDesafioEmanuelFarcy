80a283de3deb1625da094d482aac1e30c733ce06
Emanuel Farcy <emanuelfarcy@gmail.com>
2022-06-11 16:08:07 -0300

    Poniendo en marcha React

---
+++ js/main.js
@@ -0,0 +1,58 @@
+//Esto es un comentario//
+/*Me permite
+realizar comentarios claros
+en diferentes líneas*/
+
+//Ya no se utiliza más//
+//var a;//
+
+//
+//DE esta mnera te da más control en las variables//
+let numeroUno;
+
+numeroUno= 1;
+numeroUno=56;
+//let nombre= "Ema";
+
+const PI= 3.141516;
+//console.log(PI);
+
+//let nombreUsuario= prompt
+//('Ingresa tu nombre: ');
+//alert("Hola " + nombreUsuario);
+//console.log("Hola " + nombreUsuario);
+
+//Actividad 1//
+//let nombre= "Homero";
+/*let apellido= "Simpson";
+let edad= 39;
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+
+/*Actividad 2//
+const CIUDAD_1= "Córdoba";
+const CIUDAD_2= "Buenos Aires";
+const CIUDAD_3= "Mendoza";
+const CIUDAD_4= "San Juan";
+const CIUDAD_5= "Salta";
+
+console.log(CIUDAD_1);
+console.log(CIUDAD_2);
+console.log(CIUDAD_3);
+console.log(CIUDAD_4);
+console.log(CIUDAD_5);*/
+
+
+/*Actividad 3*/
+
+const nombreConductor= "Bart Simpson";
+const direccionConductor= "742 Evergreen Terr. Springfield";
+const paisConductor= "Estados Unidos";
+
+console.log("Datos del titular del carnet de conducir:", "  Nombre: ", nombreConductor  + "  Dirección: ", direccionConductor +  "  País de residencia: ", paisConductor);
+
+
+
+
